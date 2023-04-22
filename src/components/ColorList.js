585fdf5ef5399ba532407360ffca022c61feb2a8
Kennedy <kiplangatkennedy019@gmail.com>
2023-04-22 08:40:30 +0300

    done

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,18 +12,28 @@ function ColorList() {
     "hotpink",
   ];
 
-  return (
+  const colorElements = colors.map((color) => {
+    return <div><ColorItem key={color} color={color} 
+    />
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
-  );
+    </div>;
+  });
+  // etc
 }
-
+//   return (
+//     <div>
+//       <h1>Top 5 CSS Colors</h1>
+//       <ol>
+//         {/* display the array of <li> elements here! */}
+//         {colorElements}
+//       </ol>
+//     </div>
+//   );
+// }
 export default ColorList;
